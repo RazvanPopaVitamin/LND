d728359eb442f52ba3dcb55e54d2d015cad5fa46
RazvanPopaVitamin <razvan.popa@vitaminsoftware.com>
2024-02-02 14:23:51 +0200

    Setup project and general navigation
    Setup initial project, add react navigation and setup native stack and bottom tabs navigation

---
+++ screens/AllExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function AllExpenses() {
+  return <Text>1</Text>;
+}
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+function RecentExpenses() {
+  return <Text>13</Text>;
+}
+
+export default RecentExpenses;
